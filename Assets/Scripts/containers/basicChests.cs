45c7746223e3c686b343125de82afdfca27c6193
Tim-den-Bode <330583@student.mboutrecht.nl>
2024-10-23 11:07:41 +0200

    update
    Updated the player with an audio listener instead of the camera and added different sounds to different map parts.

--- Assets/Scripts/containers/basicChests.cs
+++ Assets/Scripts/containers/basicChests.cs
@@ -4,19 +4,20 @@ using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
 
-public class basicChest : MonoBehaviour , IInteractable
+public class basicChest : MonoBehaviour, IInteractable
 {
     private GameObject HUD;
     private HUD HUDScript;
     public TextMeshProUGUI interactText;
+    public GameObject father;
 
     public void Start()
     {
         HUD = GameObject.Find("HUD");
-            if (HUD == null)
-            {
-                Debug.LogError("HUD niet gevonden in de scene!");
-            }
+        if (HUD == null)
+        {
+            Debug.LogError("HUD niet gevonden in de scene!");
+        }
         HUDScript = HUD.GetComponent<HUD>();
     }
     public void Interact()
@@ -28,9 +29,9 @@ public class basicChest : MonoBehaviour , IInteractable
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player")) 
+        if (other.CompareTag("Player"))
         {
-            interactText.gameObject.SetActive(true); 
+            interactText.gameObject.SetActive(true);
         }
     }
 
@@ -38,40 +39,7 @@ public class basicChest : MonoBehaviour , IInteractable
     {
         if (other.CompareTag("Player"))
         {
-            interactText.gameObject.SetActive(false); 
-        }
-    }
-
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.CompareTag("Player")) 
-        {
-            interactText.gameObject.SetActive(true); 
-        }
-    }
-
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.CompareTag("Player"))
-        {
-            interactText.gameObject.SetActive(false); 
-        }
-    }
-
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.CompareTag("Player")) 
-        {
-            interactText.gameObject.SetActive(true); 
-        }
-    }
-
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.CompareTag("Player"))
-        {
-            interactText.gameObject.SetActive(false); 
+            interactText.gameObject.SetActive(false);
         }
     }
 }
-
